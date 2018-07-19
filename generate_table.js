6c88cab041a615f01fea6b303e123340561cb1d9
William Emfinger <waemfinger@gmail.com>
2018-07-19 10:49:37 -0500

    refactoring code working on generating table.

---
+++ generate_table.js
@@ -0,0 +1,13 @@
+const gh = require('./github');
+
+const _ = require('underscore');
+
+module.exports = {
+	generateTable
+};
+
+// takes as input a list of labels - finds all the issues that have
+// those labels and puts them into a table
+function generateTable(labels) {
+	
+}
---
+++ github.js
@@ -0,0 +1,66 @@
+const GitHub = require('github-api');
+
+let GITHUB_USERNAME = process.env.GITHUB_USERNAME;
+let GITHUB_PASSWORD = process.env.GITHUB_PASSWORD;
+
+if (!GITHUB_USERNAME || !GITHUB_PASSWORD) {
+	// DO SOMETHING HERE
+}
+
+let gh = new GitHub({
+	username: GITHUB_USERNAME,
+	password: GITHUB_PASSWORD
+});
+
+// utility functions
+function buildFilters(args) {
+	let filters = [];
+	filters.push({ key: 'state', value: args.state });
+	filters = filters.concat(args.labels.map(l => { return { key: 'labels', value: l }; }));
+	return filters;
+}
+
+function issueContainsLabel(issue, label) {
+	return issue.labels.reduce((a, e) => {
+		return a || e.name == label;
+	}, false);
+}
+
+function scrapeIssues(opts) {
+	let owner = opts.owner;
+	let repo = opts.repo;
+	let listOpts = {
+		state: 'all',
+		labels: opts.filters.filter((f) => f.key == 'labels').map((f) => f.value).join()
+	};
+	console.log(listOpts);
+	return gh.getIssues(owner, repo).listIssues(listOpts).then((issues) => {
+		issues = issues.data;
+		return issues;
+	}).catch((err) => {
+		console.log(`Couldn not get issues: ${err}`);
+		return [];
+	});
+}
+
+function printIssue(issue) {
+	let i = {
+		title: issue.title,
+		number: issue.number,
+		labels: issue.labels.map(l => l.name),
+		state: issue.state,
+		created: issue.created_at,
+		last_updated: issue.updated_at,
+		closed: issue.closed_at
+	};
+	console.log(i);
+}
+
+// what we're exporting:
+module.exports = {
+	gh,
+	buildFilters,
+	issueContainsLabel,
+	scrapeIssues,
+	printIssue
+};
