7a77c27ee9ad094b8ab877cdade5162ae848e238
William Emfinger <waemfinger@gmail.com>
2018-07-19 12:21:21 -0500

    updated sorting of labels to use natural comparison instead of strict string comparison.

--- generate_table.js
+++ generate_table.js
@@ -57,6 +57,21 @@ tableTemplate = handlebars.compile([
 	"</table>",
 ].join('\n'));
 
+function naturalCompare(a, b) {
+	var ax = [], bx = [];
+
+	a.replace(/(\d+)|(\D+)/g, function(_, $1, $2) { ax.push([$1 || Infinity, $2 || ""]) });
+	b.replace(/(\d+)|(\D+)/g, function(_, $1, $2) { bx.push([$1 || Infinity, $2 || ""]) });
+	while(ax.length && bx.length) {
+		var an = ax.shift();
+		var bn = bx.shift();
+		var nn = (an[0] - bn[0]) || an[1].localeCompare(bn[1]);
+		if(nn) return nn;
+	}
+
+	return ax.length - bx.length;
+}
+
 function generateMap(owner, repo, labels) {
 	let map = {};
 	var tasks = labels.map((l) => {
@@ -81,7 +96,7 @@ function makeRequirements(map) {
 			issues: map[k]
 		};
 	}).sort((a,b) => {
-		return a.label.localeCompare(b.label);
+		return naturalCompare(a.label, b.label);
 	});
 }
 
