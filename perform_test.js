46397e2aaf256a7f8fc8a7309df5e99b78de408d
William Emfinger <waemfinger@gmail.com>
2018-07-23 16:20:15 -0500

    added notes field to report and updated file handling and loading of requirements.

--- perform_test.js
+++ perform_test.js
@@ -11,19 +11,56 @@ const gh = require('./github');
 const files = require('./lib/files');
 const inquirer  = require('./lib/inquirer');
 
-//vars
+// where we save the reports
 const dir = 'reports'
+// where we save the requirement labels
 const reqFileName = 'requirements.json';
-let requirements = [];
 
-// check directory
-if (!files.directoryExists(dir)) {
-	console.log(chalk.red('No '+dir+' folder found - making a new one!'));
-	files.makeDirectory(dir);
-	process.exit();
+const getReports = async () => {
+	let reports = [];
+	// check directory
+	if (!files.directoryExists(dir)) {
+		console.log(chalk.red('No '+dir+' folder found - making a new one!'));
+		files.makeDirectory(dir);
+	} else {
+		const filelist = fs.readdirSync(dir);
+		reports = filelist.map((fname) => {
+			return JSON.parse(fs.readFileSync(dir + '/' + fname));
+		});
+	}
+	return reports;
+}
+
+const getRequirements = async () => {
+	let reqs = [];
+	if (!fs.existsSync(reqFileName)) {
+		console.log("Couldn't find " + reqFileName + ", scraping github!");
+		reqs = await gh.scrapeLabels({
+			owner: 'PushTracker',
+			repo: 'EvalApp',
+			patterns: {
+				labels: 'SEA-SRS'
+			}
+		});
+		reqs = reqs.map((l) => {
+			return {
+				name: l.name + ': ' + l.description,
+				short: l.name,
+				value: l.name
+			}
+		});
+		console.log("Wrote " + reqFileName);
+		fs.writeFileSync(reqFileName, JSON.stringify(reqs));
+		console.log("Wrote " + reqFileName);
+	} else {
+		reqs = JSON.parse(fs.readFileSync(reqFileName));
+	}
+	return reqs;
 }
 
 const run = async () => {
+	let requirements = await getRequirements();
+	let reports = await getReports();
 	const report = await inquirer.makeTestReport(requirements);
 	let fname = dir + "/SEA-SVR-" + report.testNumber + "-" + report.revision + ".json";
 	if (fs.existsSync(fname)) {
@@ -35,22 +72,5 @@ const run = async () => {
 	console.log("Wrote report " + fname);
 }
 
-const filelist = fs.readdirSync(dir);
-
-const reqs = gh.scrapeLabels({
-	owner: 'PushTracker',
-	repo: 'EvalApp',
-	patterns: {
-		labels: 'SEA-SRS'
-	}
-}).then((labels) => {
-	requirements = labels.map((l) => {
-		return {
-			name: l.name + ': ' + l.description,
-			short: l.name,
-			value: l.name
-		}
-	});
-	fs.writeFileSync(reqFileName, JSON.stringify(requirements));
-	run();
-})
+// now run the code
+run();
